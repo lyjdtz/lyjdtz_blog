7a7a1c4bc4fa47be8a11c1aa80ec96a5e4a9964a
1542761533 <1542761533@qq.com>
2025-09-22 16:15:52 +0800

    25.9.22

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://lyjdtz.life/",
+	site: "https://www.lyjdtz.life/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
